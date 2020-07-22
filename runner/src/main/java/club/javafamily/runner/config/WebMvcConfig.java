c58debadb1078bee8c1985ca1397c97ac4aaa1d5
DreamLi <18829346477@163.com>
2020-07-22 18:05:28 +0800

    init project

--- runner/src/main/java/club/javafamily/runner/config/WebMvcConfig.java
+++ runner/src/main/java/club/javafamily/runner/config/WebMvcConfig.java
@@ -1,17 +1,28 @@
 package club.javafamily.runner.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
    @Override
    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("app/**").addResourceLocations("file://" + webDist + "/app");
+      LOGGER.info("Registry web resource in: " + webDist);
+      registry.addResourceHandler("/app/**").addResourceLocations("file:" + webDist + "/app/");
    }
 
-   @Value("jfoa.web.dist")
+   @Override
+   public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/index.html").setViewName("index");
+      registry.addViewController("/login.html").setViewName("login");
+   }
+
+   @Value("${jfoa.web.dist}")
    private String webDist;
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(WebMvcConfig.class);
 }
