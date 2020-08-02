8b4847967a65171c0dbc31e681ac872389712a49
DreamLi <18829346477@163.com>
2020-08-02 20:51:20 +0800

    for Password Security

---
+++ runner/src/test/java/club/javafamily/runner/util/TestTools.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019, JavaFamily Technology Corp, All Rights Reserved.
+ *
+ * The software and information contained herein are copyrighted and
+ * proprietary to AngBoot Technology Corp. This software is furnished
+ * pursuant to a written license agreement and may be used, copied,
+ * transmitted, and stored only in accordance with the terms of such
+ * license and with the inclusion of the above copyright notice. Please
+ * refer to the file "COPYRIGHT" for further copyright and licensing
+ * information. This software and information or any other copies
+ * thereof may not be provided or otherwise made available to any other
+ * person.
+ */
+
+package club.javafamily.runner.util;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestTools {
+
+   @Autowired
+   private StringEncryptor encryption;
+
+   @Test
+   public void generatorEnvPassword() {
+      String password = "";
+
+      System.out.println(encryption.encrypt(password));
+   }
+}
