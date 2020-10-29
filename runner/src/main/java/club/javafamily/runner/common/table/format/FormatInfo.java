15a03e16c2d1707fc40b23f5b5ee1053274d9b72
DreamLi1314 <javafamily.club@outlook.com>
2020-10-29 19:22:46 +0800

    code review.

--- runner/src/main/java/club/javafamily/runner/common/table/format/FormatInfo.java
+++ runner/src/main/java/club/javafamily/runner/common/table/format/FormatInfo.java
@@ -1,30 +1,38 @@
 package club.javafamily.runner.common.table.format;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FormatInfo {
 
    public FormatModel getCellFormat(int row, int col) {
-      return null;
+      return formatMap.get(buildKey(row, col));
    }
 
    public void setCellFormat(int row, int col, FormatModel formatModel) {
-
+      formatMap.put(buildKey(row, col), formatModel);
    }
 
    public FormatModel getRowFormat(int row) {
-      return null;
+      return formatMap.get(buildKey(row, -1));
    }
 
    public void setRowFormat(int row, FormatModel formatModel) {
-
+      formatMap.put(buildKey(row, -1), formatModel);
    }
 
    public FormatModel getColFormat(int col) {
-      return null;
+      return formatMap.get(buildKey(-1, col));
    }
 
    public void setColFormat(int col, FormatModel formatModel) {
-
+      formatMap.put(buildKey(-1, col), formatModel);
    }
 
+   private String buildKey(int row, int col) {
+      return row + "-" + col;
+   }
+
+   private Map<String, FormatModel> formatMap = new HashMap<>();
 }
 
