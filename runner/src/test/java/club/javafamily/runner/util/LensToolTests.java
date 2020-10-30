1865875546c7e380af25e7563edb7bc179d12c99
DreamLi1314 <javafamily.club@outlook.com>
2020-10-30 17:41:53 +0800

    code review.

--- runner/src/test/java/club/javafamily/runner/util/LensToolTests.java
+++ runner/src/test/java/club/javafamily/runner/util/LensToolTests.java
@@ -5,6 +5,7 @@ import com.itextpdf.kernel.font.PdfFontFactory;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 import static club.javafamily.runner.common.table.lens.LensTool.DEFAULT_HEADER_FONT;
+import static club.javafamily.runner.common.table.lens.LensTool.DEFAULT_TEXT_FONT;
 
 public class LensToolTests {
 
@@ -33,7 +35,12 @@ public class LensToolTests {
       System.out.println(DEFAULT_HEADER_FONT.getFamily());
 
       PdfFont font = PdfFontFactory.createRegisteredFont(DEFAULT_HEADER_FONT.getName());
-      int width = font.getWidth("aaaaa");
+      Assertions.assertNotNull(font, "default header font can't convert to pdfFont");
+
+      font = PdfFontFactory.createRegisteredFont(DEFAULT_TEXT_FONT.getName());
+      Assertions.assertNotNull(font, "default text font can't convert to pdfFont");
+
+      int width = font.getWidth("jfoa");
       System.out.println(width);
    }
 
