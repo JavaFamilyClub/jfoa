d85fb2b5a39a017d200bd17f4ca8eff19d56ed64
DreamLi1314 <javafamily.club@outlook.com>
2020-12-15 13:38:23 +0800

    lint

--- web/angular/src/app/em/setting/em-setting.module.ts
+++ web/angular/src/app/em/setting/em-setting.module.ts
@@ -37,7 +37,7 @@ import { RoleManagerComponent } from "./role-manager/role-manager.component";
 import { EmSecurityManagerModule } from "./security-manager/em-security-manager.module";
 import { UserManagerComponent } from "./user-manager/user-manager.component";
 import { RoleEditViewComponent } from "./role-manager/role-edit-view/role-edit-view.component";
-import { RoleAssignedToCardComponent } from './role-manager/role-edit-view/role-assigned-to-card/role-assigned-to-card.component';
+import { RoleAssignedToCardComponent } from "./role-manager/role-edit-view/role-assigned-to-card/role-assigned-to-card.component";
 
 @NgModule({
    declarations: [
