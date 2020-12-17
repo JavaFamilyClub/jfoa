ee7ccde4b489a9aebaf5cd7c554b27baa649fa12
DreamLi1314 <javafamily.club@outlook.com>
2020-12-17 10:16:20 +0800

    fix sandbox issue.

--- runner/src/main/java/club/javafamily/runner/config/Swagger2Config.java
+++ runner/src/main/java/club/javafamily/runner/config/Swagger2Config.java
@@ -14,11 +14,13 @@
 
 package club.javafamily.runner.config;
 
+import club.javafamily.commons.utils.Tool;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -39,8 +41,9 @@ public class Swagger2Config {
    private ApiInfo apiInfo() {
       return new ApiInfoBuilder()
          .title("JavaFamily OA APIs")
-         .version("1.0")
+         .version("0.1.0")
          .description("JavaFamily OA RESTful APIs")
+         .contact(new Contact("JavaFamily", Tool.USER_DOCUMENT_URI, "javafamily.club@outlook.com"))
          .build();
    }
 }
