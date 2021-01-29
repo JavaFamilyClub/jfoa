3b0cddf842650517b458472fab8245c1f9c342c6
DreamLi1314 <javafamily.club@outlook.com>
2021-01-29 16:42:29 +0800

    prepare dingtalk login.

---
+++ runner/src/main/java/club/javafamily/runner/dto/DingTalkUser.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021, JavaFamily Technology Corp, All Rights Reserved.
+ *
+ * The software and information contained herein are copyrighted and
+ * proprietary to JavaFamily Technology Corp. This software is furnished
+ * pursuant to a written license agreement and may be used, copied,
+ * transmitted, and stored only in accordance with the terms of such
+ * license and with the inclusion of the above copyright notice. Please
+ * refer to the file "COPYRIGHT" for further copyright and licensing
+ * information. This software and information or any other copies
+ * thereof may not be provided or otherwise made available to any other
+ * person.
+ */
+
+package club.javafamily.runner.dto;
+
+import club.javafamily.commons.enums.Gender;
+import club.javafamily.commons.enums.UserType;
+
+public class DingTalkUser implements RestUser {
+
+   @Override
+   public String getName() {
+      return null;
+   }
+
+   @Override
+   public String getAccount() {
+      return null;
+   }
+
+   @Override
+   public String getEmail() {
+      return null;
+   }
+
+   @Override
+   public UserType getUserType() {
+      return null;
+   }
+
+   @Override
+   public Gender getGender() {
+      return null;
+   }
+}
---
+++ runner/src/main/java/club/javafamily/runner/rest/dingtalk/DingTalkProvider.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021, JavaFamily Technology Corp, All Rights Reserved.
+ *
+ * The software and information contained herein are copyrighted and
+ * proprietary to JavaFamily Technology Corp. This software is furnished
+ * pursuant to a written license agreement and may be used, copied,
+ * transmitted, and stored only in accordance with the terms of such
+ * license and with the inclusion of the above copyright notice. Please
+ * refer to the file "COPYRIGHT" for further copyright and licensing
+ * information. This software and information or any other copies
+ * thereof may not be provided or otherwise made available to any other
+ * person.
+ */
+
+package club.javafamily.runner.rest.dingtalk;
+
+import club.javafamily.commons.enums.UserType;
+import club.javafamily.runner.dto.*;
+import club.javafamily.runner.rest.QueryEngine;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Lazy
+@Service("dingTalkProvider")
+public class DingTalkProvider implements QueryEngine<DingTalkUser> {
+   @Override
+   public UserType getUserType() {
+      return null;
+   }
+
+   @Override
+   public String getAuthorizeUrl() {
+      return null;
+   }
+
+   @Override
+   public RestTemplate getRestTemplate() {
+      return null;
+   }
+
+   @Override
+   public String getAccessTokenUrl() {
+      return null;
+   }
+
+   @Override
+   public String getUserInfoUrl() {
+      return null;
+   }
+
+   @Override
+   public String getEmailUrl() {
+      return null;
+   }
+
+   @Override
+   public Class<DingTalkUser> getUserClass() {
+      return null;
+   }
+
+   @Override
+   public AccessTokenDTO buildAccessTokenDTO(String code, String state) {
+      return null;
+   }
+
+   @Override
+   public OAuthNotifyIdentifier getIdentifier(AccessTokenResponse accessTokenResponse) {
+      return null;
+   }
+}
